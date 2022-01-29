bacc0ce9dde974854e827368985343a584534b02
ashraffares <ashraffares090@gmail.com>
2022-01-29 06:41:54 +0200

    Add msg partial

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,38 +7,18 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
-
-require("@popperjs/core")
-
-import "bootstrap"
-
-// Import the specific modules you may need (Modal, Alert, etc)
-import { Tooltip, Popover } from "bootstrap"
-
-// The stylesheet location we created earlier
-require("../stylesheets/application.scss")
-
-// If you're using Turbolinks. Otherwise simply use: jQuery(function () {
-document.addEventListener("turbolinks:load", () => {
-    // Both of these are from the Bootstrap 5 docs
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
-        return new Tooltip(tooltipTriggerEl)
-    })
-
-    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
-        return new Popover(popoverTriggerEl)
-    })
-})
-
 import "@fortawesome/fontawesome-free/css/all"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+import "bootstrap"
+import "../stylesheets/application"
 
-require('stylesheets/application.scss')
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+})
+
 require("trix")
 require("@rails/actiontext")
